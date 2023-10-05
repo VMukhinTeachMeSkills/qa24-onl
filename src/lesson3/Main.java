d6c064c9bf2fb1b6908c26c3cdad9beb97540405
¨Viachaslau_Mukhin¨ <viachaslau.mukhin@expert-soft.by>
2023-10-05 21:17:25 +0300

    Conflict test branch commit message

--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -4,6 +4,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Homework done");
+        System.out.println("Homework done conflict_test branch");
     }
 }
